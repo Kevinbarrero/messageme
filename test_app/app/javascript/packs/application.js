f42bf4c1123d79a93da40ff495266de23460c9be
Ubuntu <ubuntu@ip-172-31-31-221.us-east-2.compute.internal>
2021-11-28 16:12:42 +0000

    added icon

--- test_app/app/javascript/packs/application.js
+++ test_app/app/javascript/packs/application.js
@@ -7,8 +7,11 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'jquery'
+require("semantic-ui-sass")
 require("jquery")
-require("semantic-ui")
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+$(document).on("turbolinks:load", () => $(".ui.dropdown").dropdown());
